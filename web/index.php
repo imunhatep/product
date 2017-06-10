551af171232e4ead7258fa07685983fe73de0741
Artyom Sukharev <aly.casus@gmail.com>
2017-06-10 11:38:35 +0300

    - next

--- web/index.php
+++ web/index.php
@@ -1,1 +1,7 @@
 <?php
+require_once '../vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('../src/Resources/view');
+$twig = new Twig_Environment(
+    $loader, [ 'cache' => '/path/to/compilation_cache', ]
+);
